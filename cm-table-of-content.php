b7c42fae7f6dfdf6a328d881a8b69acf3d941342
CreativeMindsSolutions <CreativeMindsSolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-22 17:37:32 +0000

    version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/cm-table-of-content/trunk@1185699 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cm-table-of-content.php
@@ -0,0 +1,94 @@
+<?php
+/*
+  Plugin Name: CM Table Of Contents
+  Plugin URI: http://cminds.com/cm-table-of-contents-pro
+  Description: Adds the "table of contents" to the pages based on the h1-h6 or custom tags.
+  Version: 1.0.1
+  Author: CreativeMindsSolutions
+  Author URI: http://cminds.com/
+ */
+
+if( !ini_get('max_execution_time') || ini_get('max_execution_time') < 300 )
+{
+    /*
+     * Setup the high max_execution_time to avoid timeouts during lenghty operations like importing big files
+     */
+    ini_set('max_execution_time', 300);
+    set_time_limit(300);
+}
+
+/**
+ * Define Plugin Version
+ *
+ * @since 1.0
+ */
+if( !defined('CMTOC_VERSION') )
+{
+    define('CMTOC_VERSION', '1.0.1');
+}
+
+/**
+ * Define Plugin name
+ *
+ * @since 1.0
+ */
+if( !defined('CMTOC_NAME') )
+{
+    define('CMTOC_NAME', 'CM Table Of Contents');
+}
+
+/**
+ * Define Plugin canonical name
+ *
+ * @since 1.0
+ */
+if( !defined('CMTOC_CANONICAL_NAME') )
+{
+    define('CMTOC_CANONICAL_NAME', 'CM Table Of Contents');
+}
+
+/**
+ * Define Plugin license name
+ *
+ * @since 1.0
+ */
+if( !defined('CMTOC_LICENSE_NAME') )
+{
+    define('CMTOC_LICENSE_NAME', 'CM Table Of Contents');
+}
+
+/**
+ * Define Plugin File Name
+ *
+ * @since 1.0
+ */
+if( !defined('CMTOC_PLUGIN_FILE') )
+{
+    define('CMTOC_PLUGIN_FILE', __FILE__);
+}
+
+/**
+ * Define Plugin release notes url
+ *
+ * @since 1.0
+ */
+if( !defined('CMTOC_RELEASE_NOTES') )
+{
+    define('CMTOC_RELEASE_NOTES', 'https://www.cminds.com/store/purchase-cm-table-of-content-plugin-for-wordpress/');
+}
+
+/**
+ * Define Plugin release notes url
+ *
+ * @since 1.0
+ */
+if( !defined('CMTOC_URL') )
+{
+    define('CMTOC_URL', 'https://www.cminds.com/store/purchase-cm-table-of-content-plugin-for-wordpress/');
+}
+
+include_once plugin_dir_path(__FILE__) . "tableOfContentsPro.php";
+register_activation_hook(__FILE__, array('CMTOC_Pro', '_install'));
+register_activation_hook(__FILE__, array('CMTOC_Pro', '_flush_rewrite_rules'));
+
+CMTOC_Pro::init();
