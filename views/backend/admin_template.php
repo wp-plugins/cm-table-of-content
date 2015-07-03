6bf95197397249d3f783a540d6cb3db6fb22d662
CreativeMindsSolutions <CreativeMindsSolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-03 09:11:33 +0000

    version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/cm-table-of-content/trunk@1191657 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/backend/admin_template.php
@@ -0,0 +1,12 @@
+<div class="wrap">
+    <h2>
+        <div id="icon-<?php echo CMTOC_MENU_OPTION; ?>" class="icon32">
+            <br />
+        </div>
+        <?php CMTOC_Pro::_e(CMTOC_NAME); ?>
+    </h2>
+
+    <?php CMTOC_Pro::cmtoc_showNav(); ?>
+
+    <?php echo $content; ?>
+</div>
